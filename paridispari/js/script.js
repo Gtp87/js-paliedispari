98c840d7cd5bc23ee3db2d08a86c5a847eaac490
Gtp87 <fabio.giannitrapani@gmail.com>
2021-11-29 17:01:54 +0100

    senza funzione

--- paridispari/js/script.js
+++ paridispari/js/script.js
@@ -4,3 +4,24 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari
 // Dichiariamo chi ha vinto (corrispondenza tra scelta pari e dispari e somma pari o dispari)
 // In un secondo momento creiamo delle funzioni per i numeri random e per il check pari dispari
+
+let userChoice = prompt('scrivi pari o dispari');
+let numberUser = parseInt(prompt('scegli un numero da 1 a 5'));
+
+// creiamo un numero random per il pc e lo sommiamo al numero scelto dall'utente
+
+let numberPC = Math.floor(Math.random() * 5) + 1;
+let sum = numberPC + numberUser;
+
+console.log('Pari o dispari? ' + userChoice, 'numero utente ' + numberUser, 'numero pc ' +  numberPC,  'somma ' + sum);
+
+// chi ha vinto?
+
+if (sum % 2 == 0 && userChoice == 'pari' || sum % 2 != 0 && userChoice == 'dispari') {
+    console.log('hai vinto');
+    alert('hai vinto');
+    
+} else {
+    console.log(' ha vinto il pc');
+    alert('ha vinto il pc');
+}
